595cba3d1a57083c20a25030f8deee87cdc5002c
Kyle Wisecarver <wiseworks.2022@gmail.com>
2022-10-09 09:24:02 -0500

    ready for transfer

--- Scrap-Soldier-StarterCode/ReactDjango_JWT_StarterCode-main/frontend/src/pages/AddCarsPage/AddCarsPage.js
+++ Scrap-Soldier-StarterCode/ReactDjango_JWT_StarterCode-main/frontend/src/pages/AddCarsPage/AddCarsPage.js
@@ -4,7 +4,7 @@ import useAuth from "../../hooks/useAuth";
 import './Home.css';
 import axios from "axios";
 
-const HomePage = () => {
+const AddCar = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
@@ -39,4 +39,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default AddCar;
